fe289b9f73372cd6b6cc1bc6cf4e0a4f919d7fbd
Olivia Jack <ojackson@gmail.com>
2017-11-12 23:27:05 -0500

    working chains

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const PatchBay = require('./lib/pb-live.js')
+const vSynth = require('./lib/v-synth.js')
 var ctx
 //var pb
 
 
 function init() {
-  const canvas = document.createElement('canvas')
-  ctx = canvas.getContext('2d')
-  canvas.width = 400
-  canvas.height = 300
-  ctx.fillStyle = "rgb("+Math.floor(Math.random()*255) +","+ Math.floor(Math.random()*255)+"," + Math.floor(Math.random()*255) +")"
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-  document.body.appendChild(canvas)
 
-  const canvasStream = canvas.captureStream()
 
-  var pbLive = new PatchBay(canvasStream)
+  var vs = new vSynth({networked: true})
+//  const canvasStream = vs.o[0].captureStream()
+
+  //var pbLive = new PatchBay(canvasStream)
 
   }
 
