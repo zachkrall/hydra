2f6e4ad7e7e40a27e7a5c1ce5c0a71d01106bfe1
Olivia Jack <ojackson@gmail.com>
2017-11-13 21:31:33 -0500

    working codemirror

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const PatchBay = require('./lib/pb-live.js')
 const vSynth = require('./lib/v-synth.js')
+const Editor = require('./lib/editor.js')
+
 var ctx
+
 //var pb
 
 
 function init() {
-
-
+  //console.log("loaded", document.getElementById('code'))
+  var editor = new Editor()
   var vs = new vSynth({networked: true})
 //  const canvasStream = vs.o[0].captureStream()
 
-  //var pbLive = new PatchBay(canvasStream)
+//  var pbLive = new PatchBay(canvasStream)
 
   }
 
